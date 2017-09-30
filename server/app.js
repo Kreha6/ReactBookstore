255e8ec8cf86640046c13f5ccc2e283189f1150c
Jakub <jakub.kraska95@gmail.com>
2017-09-30 11:25:47 +0200

    server is now accepting all client routes

--- server/app.js
+++ server/app.js
@@ -20,10 +20,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
+
+app.get('*', function(req,res){
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
