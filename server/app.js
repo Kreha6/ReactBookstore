acb66baddd0ab361485c2d00b30f4fc7cd16f841
Jakub <jakub.kraska95@gmail.com>
2017-10-01 00:09:24 +0200

    created update route

--- server/app.js
+++ server/app.js
@@ -53,6 +53,29 @@ app.delete('/books/:_id',function(req,res){
   })
 })
 
+app.put('/books/:_id',function(req,res){
+  var book = req.body;
+  var query = req.params._id;
+
+  var update = {
+    '$set':{
+      title:book.title,
+      description:book.description,
+      image:book.image,
+      price:book.price
+    }
+  };
+  //new means it will return updated document
+  var options = {new: true};
+
+  Books.findOneAndUpdate(query, update, options, function(err,books){
+    if(err){
+      throw err;
+    }
+    res.json(books);
+  })
+})
+
 //api
 
 
